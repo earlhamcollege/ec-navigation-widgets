567371d528cdbac7f3be5b9d99034eb0deadf3d9
Bradley Wood <woodbr@earlham.edu>
2019-08-28 11:07:48 -0400

    update styles

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-const request = require('request-promise');
-
 module.exports = {
   extend: 'apostrophe-widgets',
   label: 'Ec Navigation',
   construct: function (self, options) {
     self.pushAsset('stylesheet', 'always', { when: 'always' });
+    self.pushAsset('stylesheet', 'custom', { when: 'always' });
     self.pushAsset('script', 'always', { when: 'always' });
   },
 };
